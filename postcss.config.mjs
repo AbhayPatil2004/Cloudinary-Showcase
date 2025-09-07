df00db513bca977e04f0042a52aa47c15120e2d5
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2025-09-07 23:20:34 +0530

    Prisma and NeonDb Setup

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,31 @@
+// import daisyui from "daisyui";
+// import themes from "daisyui/theme/object";
+
+// const config = {
+//   plugins: ["@tailwindcss/postcss" , require("daisyui")],
+//   daisyui:[
+//     themes:["dark"] 
+//   ]
+// };
+
+// export default config;
+
+// tailwind.config.mjs
+import daisyui from "daisyui";
+
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui], // ✅ register DaisyUI
+  daisyui: {
+    themes: ["dark"], // ✅ configure themes here
+  },
 };
 
 export default config;

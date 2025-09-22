1858c31691da3a612991957ca279a7a719527229
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2025-09-22 23:24:13 +0530

    Rename PostCSS/Tailwind config files and other updates

--- postconfig.mjs
+++ postconfig.mjs
@@ -32,22 +32,22 @@
 
 
 // tailwind.config.mjs
-import daisyui from "daisyui";
+// import daisyui from "daisyui";
 
-/** @type {import('tailwindcss').Config} */
-const config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [daisyui],
-  daisyui: {
-    themes: ["dark"], // ✅ correct way
-  },
-};
+// /** @type {import('tailwindcss').Config} */
+// const config = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [daisyui],
+//   daisyui: {
+//     themes: ["dark"], 
+//   },
+// };
 
-export default config;
+// export default config;

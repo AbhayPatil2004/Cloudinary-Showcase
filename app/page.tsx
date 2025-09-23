dcead848bb607890d12e75e5f2d289adcfa6a3b6
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2025-09-23 19:07:29 +0530

    Update Video card

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
 import { SignUp } from "@clerk/nextjs";
 
 
-export default function page(){
+export default function page() {
     return (
-        <div className="h-full flex justify-center items-center">
-            <SignUp/>
+        <div>
+            <div>
+                <div className="flex items-center justify-center w-full p-5 mb-10 bg-gray-900 text-3xl text-white">
+                    Cloudinary Showcase
+                </div>
+            </div>
+
+            <div className="flex min-h-screen items-center justify-center ">
+                <div className="w-full max-w-md flex justify-center">
+                    <SignUp
+                        appearance={{
+                            elements: {
+                                rootBox: "mx-auto flex justify-center",
+                                card: "shadow-xl rounded-2xl",
+                            },
+                        }}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
